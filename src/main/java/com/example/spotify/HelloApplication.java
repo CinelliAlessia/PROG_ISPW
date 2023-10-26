e160ab7eceea7cc1ee3235dc96980bc457bce398
andreatozzi99 <andreatozzi199@gmail.com>
2023-10-26 11:57:40 +0200

    Commit e Push di Andrea tolto un print

--- src/main/java/com/example/spotify/HelloApplication.java
+++ src/main/java/com/example/spotify/HelloApplication.java
@@ -12,7 +12,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         System.out.println("Prova");
-        System.out.println("Prov a");
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
