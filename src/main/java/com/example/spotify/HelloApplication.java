5562dba2b54902737f7c206a3ed9c153f133e5a8
mzlala <cinellialessia@gmail.com>
2023-10-26 11:38:33 +0200

    2 Print

--- src/main/java/com/example/spotify/HelloApplication.java
+++ src/main/java/com/example/spotify/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         System.out.println("Prova");
+        System.out.println("Prova");
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
