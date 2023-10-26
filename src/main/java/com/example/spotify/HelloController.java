d01034f3ef847597bf1e3bb2de208f0991ca5ea3
mzlala <cinellialessia@gmail.com>
2023-10-26 10:52:17 +0200

    Prova

---
+++ src/main/java/com/example/spotify/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.spotify;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
+
+public class HelloController {
+    public Button login;
+    public PasswordField password;
+    public TextField username;
+    @FXML
+    private Label textLogin;
+
+    @FXML
+    protected void onLoginClick() {
+        if(Objects.equals(username.getText(), "admin")){
+            if(Objects.equals(password.getText(), "1234")){
+                textLogin.setText("Credenziali corrette");
+            }
+        }
+
+    }
+
+}
