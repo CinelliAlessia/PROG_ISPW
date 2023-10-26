11a5e8a33c7d9b71e88e269b0eb22bf66e605cfa
mzlala <cinellialessia@gmail.com>
2023-10-26 12:06:37 +0200

    Commit and push di Alessia

--- src/main/java/com/example/spotify/HelloApplication.java
+++ src/main/java/com/example/spotify/HelloApplication.java
@@ -11,9 +11,10 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println("Prova Alessia");
+        System.out.println("Prova Alessia ciao");
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
+        System.out.println("Da mantenere");
         stage.setTitle("Access Login");
         stage.setScene(scene);
         stage.show();
