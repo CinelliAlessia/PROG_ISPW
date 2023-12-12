9671ac1ff391ca4ef26c7d8f2e8a3df5348c4ce4
mzlala <cinellialessia@gmail.com>
2023-12-12 14:48:05 +0100

    Inizio progetto:

--- src/main/java/com/example/spotify/Login.java
+++ src/main/java/com/example/spotify/Login.java
@@ -13,7 +13,7 @@ public class Login extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 750, 750);
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
         stage.setTitle("Access Login");
         stage.setScene(scene);
         stage.show();
