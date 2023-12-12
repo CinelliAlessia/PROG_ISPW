002301a8e821fe0b6dae0910ae6696d86e5a5462
mzlala <cinellialessia@gmail.com>
2023-12-12 21:18:05 +0100

    Aggiunta della pagina e controller grafico Home

--- src/main/java/logic/LoginCtrlGrafico.java
+++ src/main/java/logic/LoginCtrlGrafico.java
@@ -35,7 +35,7 @@ public class LoginCtrlGrafico {
             textLogin.setText("Credenziali corrette");
             // ora dovrei permettere "all'utente" di visualizzare la homePage
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/registrazione.fxml")); // DA CAMBIARE VIEW
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/homePage.fxml")); // DA CAMBIARE VIEW
                 Scene scene = new Scene(loader.load());
                 Stage stage = (Stage) login.getScene().getWindow(); // Ottieni la finestra corrente
                 stage.setScene(scene);
