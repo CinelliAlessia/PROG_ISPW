9655270318f195844738d250f794ffb47f75cdf0
mzlala <cinellialessia@gmail.com>
2023-12-12 22:24:25 +0100

    home page implementazione start

--- src/main/java/logic/HomePageCtrlGrafico.java
+++ src/main/java/logic/HomePageCtrlGrafico.java
@@ -1,4 +1,19 @@
 package logic;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class HomePageCtrlGrafico {
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/view/homePage.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("Home Page");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
