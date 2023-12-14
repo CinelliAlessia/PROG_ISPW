c5a94ed92807cb6cc0fd43bb6b1bb4a3da128475
mzlala <cinellialessia@gmail.com>
2023-12-14 12:10:49 +0100

    modifica grafiche

--- src/main/java/logic/RegistrazioneCtrlGrafico.java
+++ src/main/java/logic/RegistrazioneCtrlGrafico.java
@@ -1,12 +1,16 @@
 package logic;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class RegistrazioneCtrlGrafico {
+
+    public Button back;
 
     public  void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/view/registrazione.fxml"));
@@ -16,6 +20,13 @@ public class RegistrazioneCtrlGrafico {
         stage.setTitle("Registrazione");
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    protected void onBackClick() throws IOException {
+        Stage stage = (Stage) back.getScene().getWindow();
+        LoginCtrlGrafico loginCtrlGrafico = new LoginCtrlGrafico();
+        loginCtrlGrafico.start(stage);
     }
 
 }
