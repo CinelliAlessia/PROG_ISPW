7c6a4d2d6de545eed533101c440250ea6878ad11
mzlala <cinellialessia@gmail.com>
2024-02-02 12:29:58 +0100

    Inizio implementazione Observer

---
+++ src/main/java/engineering/pattern/observer/Observer.java
@@ -0,0 +1,9 @@
+package engineering.pattern.observer;
+
+
+/** Definisce l'interfaccia per tutti i subscribers per ricevere le notifiche dai publisher
+ * Verrà usata come classe astratta implementata dai ConcreteObserver che realizzeranno update */
+public interface Observer {
+
+    void update();
+}
---
+++ src/main/java/engineering/pattern/observer/Subject.java
@@ -0,0 +1,39 @@
+package engineering.pattern.observer;
+
+import java.util.ArrayList;
+
+/** Subject è una classe astratta che fornisce interfacce per registrare o rimuovere dinamicamente gli observer (i subscribers)
+ * implementa le seguenti funzioni:
+ * 1. attach(observer): aggiunge un ConcreteObserver alla lista delle classi da notificare
+ * 2. detach(observer): rimuove un ConcreteObserver alla lista delle classi da notificare
+ * 3. notify(): notifica un cambiamento alle classi ConcreteObserver
+ * */
+public abstract class Subject {
+
+    private ArrayList<Observer> observers ;
+
+    protected Subject() {
+        observers = new ArrayList<>() ;
+    }
+
+    public void attach(Observer newObserver) {
+        observers.add(newObserver) ;
+    }
+
+    public void detach(Observer removeObserver) {
+        observers.remove(removeObserver) ;
+    }
+
+    /** Implementato con un loop su tutti i ConcreteObserver, dove ciascuno di essi chiama la funzione update()
+     * Viene eseguita dal ConcreteSubject (il publisher) per notificare il suo cambio di stato
+
+     * È BUONA NORMA RENDERLO PRIVATO PERCHÈ SOLO CONCRETEOBSERVER DOVREBBE CHIAMARLO */
+    protected void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+
+
+}
