acef18e531a0a718ed30486d73b1b52734bd9680
mzlala <cinellialessia@gmail.com>
2024-02-10 23:58:40 +0100

    Model Notice

---
+++ src/main/java/model/Notice.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Notice {
+    private String title;
+    private String body;
+
+    public Notice(String title, String body){
+        setTitle(title);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+
+}
