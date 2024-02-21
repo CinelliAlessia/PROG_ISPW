1f6d60d597201e97ea7f322ada4fdbff1ea45efc
andreatozzi99 <andreatozzi199@gmail.com>
2024-02-21 12:52:01 +0100

    Pop-up corretto

--- src/main/java/view/first/utils/MessageString.java
+++ src/main/java/view/first/utils/MessageString.java
@@ -2,7 +2,7 @@ package view.first.utils;
 
 public class MessageString {
     private MessageString(){}
-    public static final String ADDED_PENDING_PLAYLIST= "We are reviewing your playlist for approval";
+    public static final String ADDED_PENDING_PLAYLIST= "Playlist under review for approval";
     public static final String ADDED_PLAYLIST= "Your playlist has been added";
-    public static final String UPDATED_PREFERNCES= "Preferences updated!";
+    public static final String UPDATED_PREFERNCES= "Preferences updated";
 }
